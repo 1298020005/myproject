2bfd776c92009e0b44404baa7c26f442314c1027
杨一帆 <yangyifan@boswinner.com>
2021-12-24 18:58:48 +0800

    未完成

--- src/main/java/com/example/myproject/leetcode/easy/onehundred/SeventyOne.java
+++ src/main/java/com/example/myproject/leetcode/easy/onehundred/SeventyOne.java
@@ -2,8 +2,14 @@ package com.example.myproject.leetcode.easy.onehundred;
 
 public class SeventyOne {
     public int titleToNumber(String columnTitle) {
-        int res;
-        return res;
+        int res = 0;
+        StringBuffer b=new StringBuffer();
+        char[] chars=b.append(columnTitle).reverse().toString().toCharArray();
+        for (char c:chars){
+            System.out.println(c);
+            res+=c-65+26;
+        }
+        return res-26;
     }
 
     public static void main(String[] args) {
