fe766de05bef446d3704fa8da7f5656d060de14e
杨一帆 <yangyifan@boswinner.com>
2021-12-27 19:33:36 +0800

    12-27 日内容

--- src/main/java/com/example/myproject/leetcode/easy/twohundred/TwentyEight.java
+++ src/main/java/com/example/myproject/leetcode/easy/twohundred/TwentyEight.java
@@ -5,12 +5,25 @@ import java.util.List;
 
 public class TwentyEight {
     public List<String> summaryRanges(int[] nums) {
-        ArrayList<String> res=new ArrayList<>();
+        ArrayList<String> res = new ArrayList<>();
+        int temp = 0;
+        int start=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                temp = nums[i];
+            } else {
+                if (nums[i] == ++temp) {
+                    start=
+                }
+            }
+
+        }
         return res;
     }
+
     public static void main(String[] args) {
-        TwentyEight twentyEight=new TwentyEight();
-        int[] nums=new int[]{0,1,2,4,5,7};
+        TwentyEight twentyEight = new TwentyEight();
+        int[] nums = new int[]{0, 1, 2, 4, 5, 7};
         twentyEight.summaryRanges(nums);
     }
 }
